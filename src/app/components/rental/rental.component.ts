3a12e862916dd001e04f226679c27bf217a2596f
Enes Eriş <eneseris123@hotmail.com>
2021-03-28 19:15:20 +0300

    ders 17

---
+++ src/app/components/rental/rental.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Rental } from 'src/app/models/rental';
+import { RentalService } from 'src/app/services/rental.service';
+
+@Component({
+  selector: 'app-rental',
+  templateUrl: './rental.component.html',
+  styleUrls: ['./rental.component.css']
+})
+export class RentalComponent implements OnInit {
+
+  rentals : Rental[]=[];
+  dataLoaded=false;
+
+  constructor(private rentService:RentalService) { }
+
+  ngOnInit(): void {
+    this.getRentals()
+  }
+
+  getRentals(){
+    this.rentService.getRentals().subscribe(response=> {
+      this.rentals=response.data;
+      this.dataLoaded=true;
+    })
+    console.log()
+  }
+
+}
---
+++ src/app/models/rental.ts
@@ -0,0 +1,9 @@
+export interface Rental{
+    rentalId:number
+    carName:string
+    userName:string
+    companyName:string
+    rentDate:string
+    returnDate:string
+
+}
